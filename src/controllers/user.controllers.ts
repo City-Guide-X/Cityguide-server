e190ec1b4589f2adea0889b2886fe91e9bdab74b
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-13 19:41:30 +0100

    Clean up the import statements

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -4,7 +4,6 @@ import {
   addFavouritePropertyInput,
   createUserInput,
   loginUserInput,
-  removeAccommodationInput,
   removeFavouritePropertyInput,
   updateUserInput,
   upgradeUserToPartnerInput,
