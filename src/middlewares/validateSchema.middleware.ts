73fbdab6f9707ecca462788fee8c8776fdf041a0
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-22 19:52:50 +0100

    Add log update to check what the stay reservation error is coming from

--- src/middlewares/validateSchema.middleware.ts
+++ src/middlewares/validateSchema.middleware.ts
@@ -5,6 +5,7 @@ import { ErrorMessageOptions, generateError } from 'zod-error';
 
 const validateSchema = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
   try {
+    console.log(req.body);
     schema.parse({
       body: req.body,
       query: req.query,
