c3bc09c05bfb574d308afe8305f417be5aac7aa3
Toby2507 <oluwatobisalau2000@gmail.com>
2024-08-23 04:25:08 +0100

    Remove the console.log in the validateSchema middleware since the body issue has been fixed

--- src/middlewares/validateSchema.middleware.ts
+++ src/middlewares/validateSchema.middleware.ts
@@ -5,7 +5,6 @@ import { ErrorMessageOptions, generateError } from 'zod-error';
 
 const validateSchema = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(req.body);
     schema.parse({
       body: req.body,
       query: req.query,
