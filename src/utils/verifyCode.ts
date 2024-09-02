bff68a14e34b2b8f8fe0dbda5a2d0811fbb013fe
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-02 13:41:22 +0100

    Refactor the verify code generator to eleminating leading zeros ensuring the code is always 6 digits long

--- src/utils/verifyCode.ts
+++ src/utils/verifyCode.ts
@@ -1,7 +1,7 @@
 const verifyCode = (): number => {
-  let code = '';
-  for (let i = 0; i < 6; i++) code += Math.floor(Math.random() * 10);
-  return parseInt(code);
+  let code = Math.floor(Math.random() * 9) + 1;
+  for (let i = 1; i < 6; i++) code = code * 10 + Math.floor(Math.random() * 10);
+  return code;
 };
 
 export default verifyCode;
