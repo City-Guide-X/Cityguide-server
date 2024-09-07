4e9e12384d9fc4f6991415bb749f45f9fe701273
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-07 06:46:15 +0100

    Add the search nightlifes service

--- src/services/nightlife.service.ts
+++ src/services/nightlife.service.ts
@@ -1,5 +1,6 @@
 import { NotFoundError } from '@errors';
 import { NightLife, NightLifeModel, ReservationModel, ReviewModel } from '@models';
+import { DayOfWeek } from '@types';
 
 export const createNightLife = (input: Partial<NightLife>) => {
   return NightLifeModel.create({ ...input });
@@ -33,4 +34,85 @@ export const deleteNightLife = async (_id: string, partner: string) => {
   const { deletedCount } = await NightLifeModel.deleteOne({ _id, partner });
   if (!deletedCount) throw new NotFoundError('NightLife not found');
   await Promise.all([ReviewModel.deleteMany({ property: _id }), ReservationModel.deleteMany({ property: _id })]);
+};
+
+export const searchNightlife = async (day?: DayOfWeek, time?: string, minAge?: number) => {
+  return NightLifeModel.aggregate([
+    {
+      $match: {
+        ...(minAge && { 'rules.minAge': { $gte: +minAge } }),
+        ...(time &&
+          day && {
+            $expr: {
+              $anyElementTrue: {
+                $map: {
+                  input: '$availability',
+                  as: 'avail',
+                  in: {
+                    $let: {
+                      vars: {
+                        fromTime: { $toDate: { $concat: ['2000-01-01T', '$$avail.from', ':00Z'] } },
+                        toTime: { $toDate: { $concat: ['2000-01-01T', '$$avail.to', ':00Z'] } },
+                        queryTime: { $toDate: { $concat: ['2000-01-01T', time, ':00Z'] } },
+                        isOvernight: { $gt: ['$$avail.from', '$$avail.to'] },
+                      },
+                      in: {
+                        $and: [
+                          { $eq: ['$$avail.day', day] },
+                          {
+                            $cond: {
+                              if: '$$isOvernight',
+                              then: {
+                                $and: [
+                                  { $gte: ['$$queryTime', '$$fromTime'] },
+                                  {
+                                    $lte: [
+                                      {
+                                        $dateAdd: {
+                                          startDate: '$$queryTime',
+                                          unit: 'hour',
+                                          amount: 1,
+                                        },
+                                      },
+                                      {
+                                        $dateAdd: {
+                                          startDate: '$$toTime',
+                                          unit: 'day',
+                                          amount: 1,
+                                        },
+                                      },
+                                    ],
+                                  },
+                                ],
+                              },
+                              else: {
+                                $and: [
+                                  { $gte: ['$$queryTime', '$$fromTime'] },
+                                  {
+                                    $lte: [
+                                      {
+                                        $dateAdd: {
+                                          startDate: '$$queryTime',
+                                          unit: 'hour',
+                                          amount: 1,
+                                        },
+                                      },
+                                      '$$toTime',
+                                    ],
+                                  },
+                                ],
+                              },
+                            },
+                          },
+                        ],
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          }),
+      },
+    },
+  ]);
 };
