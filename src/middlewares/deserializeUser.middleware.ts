b17c910ac6a84687d2eab5bb54a7b8ba852b3695
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-15 17:28:14 +0100

    Update the type of the response for the verify jwt function

--- src/middlewares/deserializeUser.middleware.ts
+++ src/middlewares/deserializeUser.middleware.ts
@@ -1,10 +1,11 @@
+import { IPayload } from '@types';
 import { verifyJWT } from '@utils';
 import { NextFunction, Request, Response } from 'express';
 
 const deserializeUser = async (req: Request, res: Response, next: NextFunction) => {
   const accessToken = req.headers.authorization?.split(' ')[1] || '';
   if (!accessToken) return next();
-  const decoded = verifyJWT(accessToken, 'access');
+  const decoded = verifyJWT<IPayload>(accessToken, 'access');
   if (decoded) res.locals.user = decoded;
   return next();
 };
