9e773b866b36125dbd8b4cb4f09d1eddae868f62
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-15 15:07:11 +0100

    Add the restaurant menu event to the server to client events

--- src/types/socket.interface.ts
+++ src/types/socket.interface.ts
@@ -2,7 +2,7 @@ import { Establishment, NightLife, Reservation, Restaurant, Review, Stay } from 
 import { PropertyType, Status } from './enums';
 import { Server } from 'socket.io';
 import { TAction } from './common.interface';
-import { IAccommodation } from './model.interface';
+import { IAccommodation, IMenu } from './model.interface';
 
 export interface ClientToServerEvents {
   add_user: (userId: string) => void;
@@ -13,6 +13,8 @@ export interface ClientToServerEvents {
   update_establishment: (establishment: Partial<Establishment>) => void;
 }
 export interface ServerToClientEvents {
+  ping: (val: string) => void;
+  restaurant_menu: (data: { id: string; action: TAction; menuId?: string; body?: IMenu[] | IMenu }) => void;
   stay_acc: (data: { id: string; action: TAction; accId?: string; body?: IAccommodation[] | IAccommodation }) => void;
   update_property: (data: { id: string; type: PropertyType; body: Partial<Stay | Restaurant | NightLife> }) => void;
   delete_property: (data: { id: string; type: PropertyType }) => void;
