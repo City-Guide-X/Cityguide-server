8f8ca74bb600ebd72c375a3c2e771817d5244b5d
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-15 14:31:43 +0100

    Update the server to client socket events interface

--- src/types/socket.interface.ts
+++ src/types/socket.interface.ts
@@ -1,6 +1,8 @@
-import { Establishment, Reservation, Review } from '@models';
-import { Status } from './enums';
+import { Establishment, NightLife, Reservation, Restaurant, Review, Stay } from '@models';
+import { PropertyType, Status } from './enums';
 import { Server } from 'socket.io';
+import { TAction } from './common.interface';
+import { IAccommodation } from './model.interface';
 
 export interface ClientToServerEvents {
   add_user: (userId: string) => void;
@@ -12,21 +14,16 @@ export interface ClientToServerEvents {
   update_establishment: (establishment: Partial<Establishment>) => void;
 }
 export interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
+  stay_acc: (data: { id: string; action: TAction; accId?: string; body?: IAccommodation[] | IAccommodation }) => void;
+  update_property: (data: { id: string; type: PropertyType; body: Partial<Stay | Restaurant | NightLife> }) => void;
+  delete_property: (data: { id: string; type: PropertyType }) => void;
   new_reservation: (reservation: Partial<Reservation>) => void;
   updated_reservation: (data: { reservationId: string; status: Status }) => void;
   new_review: (data: { establishmentId: string; review: Partial<Review> }) => void;
   deleted_review: (data: { establishmentId: string; reviewId: string }) => void;
   updated_establishment: (establishment: Partial<Establishment>) => void;
 }
-export interface InterServerEvents {
-  ping: () => void;
-}
-export interface SocketData {
-  name: string;
-  age: number;
-}
+export interface InterServerEvents {}
+export interface SocketData {}
 
 export type TSocket = Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
