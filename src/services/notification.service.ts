9a426a67c51d1a12a68a95f03fdc091fc37b8ab6
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-22 11:03:04 +0100

    Update the read notification service to read multiple notifications simultaneous

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -9,15 +9,17 @@ export const getUserNotifications = (recipient: string) => {
   return NotificationModel.find({ recipient });
 };
 
-export const readNotification = async (_id: string, recipient: string) => {
-  const { matchedCount, modifiedCount } = await NotificationModel.updateOne(
-    { _id, recipient },
+export const readNotification = async (ids: string[], recipient: string) => {
+  const { matchedCount, modifiedCount } = await NotificationModel.updateMany(
+    { _id: { $in: ids }, recipient },
     { $set: { isRead: true } }
   );
-  if (!matchedCount) {
-    const notification = await NotificationModel.findById(_id);
-    if (!notification) throw new NotFoundError('Notification not found');
-    if (notification.recipient) throw new BadRequestError('Notification does not belong to logged in user');
+  if (matchedCount !== ids.length) {
+    const notifications = await NotificationModel.find({ _id: { $in: ids } }, { recipient: 1 }).lean();
+    if (notifications.length < ids.length) throw new NotFoundError('Not all notifications found');
+    const invalidNotifications = notifications.filter((n) => n.recipient.toString() !== recipient);
+    if (invalidNotifications.length > 0)
+      throw new BadRequestError('Some notifications do not belong to the logged-in user');
   }
-  if (!modifiedCount) throw new BadRequestError('Notification not read successfully');
+  if (modifiedCount !== matchedCount) throw new BadRequestError('Some notifications were not read successfully');
 };
