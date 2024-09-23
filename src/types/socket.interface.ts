711e933d300bfa244cc08f17c90ddc154f3572a6
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-23 09:02:48 +0100

    Update the server to client events interface

--- src/types/socket.interface.ts
+++ src/types/socket.interface.ts
@@ -8,16 +8,16 @@ export interface ClientToServerEvents {
   add_user: (userId: string) => void;
 }
 export interface ServerToClientEvents {
-  ping: (val: string) => void;
   restaurant_menu: (data: { id: string; action: TAction; menuId?: string; body?: IMenu[] | IMenu }) => void;
   stay_acc: (data: { id: string; action: TAction; accId?: string; body?: IAccommodation[] | IAccommodation }) => void;
   update_property: (data: { id: string; type: PropertyType; body: Partial<Stay | Restaurant | NightLife> }) => void;
   delete_property: (data: { id: string; type: PropertyType }) => void;
   new_reservation: (reservation: Partial<Reservation>) => void;
   update_reservation: (data: { reservationId: string; status: Status }) => void;
-  new_review: (data: { establishmentId: string; review: Partial<Review> }) => void;
-  delete_review: (data: { establishmentId: string; reviewId: string }) => void;
+  new_review: (data: { property: string; type: PropertyType; review: Partial<Review> }) => void;
+  delete_review: (data: { property: string; type: PropertyType; reviewId: string }) => void;
   update_establishment: (establishment: Partial<Establishment>) => void;
+  new_notification: (notification: Partial<Notification>) => void;
 }
 export interface InterServerEvents {}
 export interface SocketData {}
