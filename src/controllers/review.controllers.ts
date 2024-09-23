28854e42c6874d7954d2b2d65c8dceda0c762822
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-23 09:28:29 +0100

    Update the review controller to create notification and emit socket events

--- src/controllers/review.controllers.ts
+++ src/controllers/review.controllers.ts
@@ -1,7 +1,8 @@
-import { BadRequestError } from '@errors';
+import { BadRequestError, NotFoundError } from '@errors';
 import { privateFields } from '@models';
 import { createReviewInput, deleteReviewInput, getPropertyReviewInput } from '@schemas';
-import { createReview, deleteReview, getReviews, isPropertyType } from '@services';
+import { createNotification, createReview, deleteReview, getReviews, isPropertyType } from '@services';
+import { EntityType, NotificationType } from '@types';
 import { asyncWrapper } from '@utils';
 import { Request, Response } from 'express';
 import { omit } from 'lodash';
@@ -9,11 +10,26 @@ import { omit } from 'lodash';
 export const createReviewHandler = asyncWrapper(async (req: Request<{}, {}, createReviewInput>, res: Response) => {
   const { id } = res.locals.user;
   const { property, ...body } = req.body;
-  const data = { ...body, property: property, user: id };
+  const data = { ...body, property, user: id };
   const isValid = await isPropertyType(property, data.propertyType);
   if (!isValid) throw new BadRequestError(`Invalid ${data.propertyType} ID`);
   const review = await createReview(data);
-  return res.status(201).json({ review: omit(review.toJSON(), privateFields) });
+  const reviewResponse = omit(review.toJSON(), privateFields);
+  res.status(201).json({ review: reviewResponse });
+  const populatedReview: any = await review.populate({ path: 'property', select: 'name type -_id' });
+  const notification = {
+    recipient: populatedReview.property.partner.id,
+    recipientType: EntityType.ESTABLISHMENT,
+    type: NotificationType.REVIEW,
+    title: 'New Review',
+    message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
+      populatedReview.property.name
+    } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
+  };
+  const newNotification = await createNotification(notification);
+  const socketId = onlineUsers.get(populatedReview.property.partner.id);
+  if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
+  res.locals.io?.emit('new_review', { property, type: body.propertyType, review: reviewResponse });
 });
 
 export const getPropertyReviewsHandler = asyncWrapper(async (req: Request<getPropertyReviewInput>, res: Response) => {
@@ -25,7 +41,21 @@ export const getPropertyReviewsHandler = asyncWrapper(async (req: Request<getPro
 export const deleteReviewHandler = asyncWrapper(async (req: Request<deleteReviewInput>, res: Response) => {
   const { id } = res.locals.user;
   const { reviewId } = req.params;
-  const { matchedCount, modifiedCount } = await deleteReview(reviewId, id);
-  if (!matchedCount || !modifiedCount) throw new BadRequestError();
-  return res.sendStatus(204);
+  const review = await deleteReview(reviewId, id);
+  if (!review) throw new NotFoundError('Review not found');
+  res.sendStatus(204);
+  const populatedReview: any = await review.populate({ path: 'property', select: 'name type -_id' });
+  const notification = {
+    recipient: populatedReview.property.partner.id,
+    recipientType: EntityType.ESTABLISHMENT,
+    type: NotificationType.REVIEW,
+    title: 'New Review',
+    message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
+      populatedReview.property.name
+    } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
+  };
+  const newNotification = await createNotification(notification);
+  const socketId = onlineUsers.get(populatedReview.property.partner.id);
+  if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
+  res.locals.io?.emit('delete_review', { property: review.property.id, type: review.propertyType, reviewId });
 });
