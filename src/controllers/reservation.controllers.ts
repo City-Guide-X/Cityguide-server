fbdafbd6ca78530f63bb8f84dc51544555a49b04
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-25 20:10:37 +0100

    Update the review and reservation controllers to be more atomic using mongodb transactions

--- src/controllers/reservation.controllers.ts
+++ src/controllers/reservation.controllers.ts
@@ -25,47 +25,62 @@ import { asyncWrapper } from '@utils';
 import dayjs from 'dayjs';
 import { Request, Response } from 'express';
 import { omit } from 'lodash';
+import mongoose from 'mongoose';
 
 export const createReservationHandler = asyncWrapper(
   async (req: Request<{}, {}, createReservationInput>, res: Response) => {
     const { id } = res.locals.user;
     let data: IReservation = { ...req.body, user: id };
-    await validateReservationInput(data);
-    const reservation = await createReservation(data);
-    const reservationResponse = omit(reservation.toJSON(), privateFields);
-    res.status(201).json({ reservation });
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      await validateReservationInput(data);
+      const reservation = await createReservation(data);
+      const reservationResponse = omit(reservation.toJSON(), privateFields);
+      const populatedProperty: any = await reservation.populate({
+        path: 'property',
+        select: 'type name accommodation -_id',
+      });
 
-    const populatedProperty: any = await reservation.populate({
-      path: 'property',
-      select: 'type name accommodation -_id',
-    });
-    const notification = {
-      recipient: data.partner,
-      recipientType: EntityType.ESTABLISHMENT,
-      type: NotificationType.RESERVATION,
-      title: 'New Reservation',
-      message: `A new reservation (Ref: ${reservation.reservationRef}) has been made at your ${
-        populatedProperty.property.type ?? 'Restaurant'
-      } — ${populatedProperty.property.name}! The reservation is for ${dayjs(data.checkInDay).format(
-        'dddd, MMMM D, YYYY [at] h:mm A'
-      )}, with ${data.noOfGuests.adults + data.noOfGuests.children} guest(s). Head to your dashboard for more details`,
-    };
-    const [newNotification] = await Promise.all([
-      createNotification(notification),
-      data.propertyType === PropertyType.STAY
-        ? updateAccommodationAvailability(data.property, data.accommodations!)
-        : Promise.resolve(),
-    ]);
-    const socketId = onlineUsers.get(data.partner);
-    if (socketId) {
-      res.locals.io?.to(socketId).emit('new_reservation', reservationResponse);
-      res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
-    }
-    if (data.propertyType === PropertyType.STAY) {
-      const updatedAccommodations = populatedProperty.property.accommodation.filter((a: any) =>
-        data.accommodations!.some((da) => da.accommodationId === a.id)
-      );
-      res.locals.io?.emit('stay_acc', { id: data.property, action: 'update', body: updatedAccommodations });
+      const notification = {
+        recipient: data.partner,
+        recipientType: EntityType.ESTABLISHMENT,
+        type: NotificationType.RESERVATION,
+        title: 'New Reservation',
+        message: `A new reservation (Ref: ${reservation.reservationRef}) has been made at your ${
+          populatedProperty.property.type ?? 'Restaurant'
+        } — ${populatedProperty.property.name}! The reservation is for ${dayjs(data.checkInDay).format(
+          'dddd, MMMM D, YYYY [at] h:mm A'
+        )}, with ${
+          data.noOfGuests.adults + data.noOfGuests.children
+        } guest(s). Head to your dashboard for more details`,
+      };
+      const [newNotification] = await Promise.all([
+        createNotification(notification),
+        data.propertyType === PropertyType.STAY
+          ? updateAccommodationAvailability(data.property, data.accommodations!)
+          : Promise.resolve(),
+      ]);
+      await session.commitTransaction();
+      session.endSession();
+
+      const socketId = onlineUsers.get(data.partner);
+      if (socketId) {
+        res.locals.io?.to(socketId).emit('new_reservation', reservationResponse);
+        res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
+      }
+      if (data.propertyType === PropertyType.STAY) {
+        const updatedAccommodations = populatedProperty.property.accommodation.filter((a: any) =>
+          data.accommodations!.some((da) => da.accommodationId === a.id)
+        );
+        res.locals.io?.emit('stay_acc', { id: data.property, action: 'update', body: updatedAccommodations });
+      }
+
+      return res.status(201).json({ reservation: reservationResponse });
+    } catch (err: any) {
+      await session.abortTransaction();
+      session.endSession();
+      throw err;
     }
   }
 );
@@ -105,60 +120,84 @@ export const getReservationByRefHandler = asyncWrapper(async (req: Request<reser
 export const cancelReservationHandler = asyncWrapper(async (req: Request<cancelReservationInput>, res: Response) => {
   const { id } = res.locals.user;
   const { reservationId } = req.params;
-  const reservation = await updateReservation(reservationId, false, id, { status: Status.CANCELLED });
-  if (!reservation) throw new NotFoundError('Reservation not found');
-  const populatedProperty: any = await reservation.populate({
-    path: 'property',
-    select: 'type name -_id',
-  });
-  const notification = {
-    recipient: reservation.partner,
-    recipientType: EntityType.ESTABLISHMENT,
-    type: NotificationType.RESERVATION,
-    title: 'Reservation Cancelled',
-    message: `A reservation (Ref: ${reservation.reservationRef}) at your ${
-      populatedProperty.property.type ?? 'Restaurant'
-    } — ${populatedProperty.property.name} has been cancelled. The reservation was for ${dayjs(
-      reservation.checkInDay
-    ).format('dddd, MMMM D, YYYY [at] h:mm A')}, with ${
-      reservation.noOfGuests.adults + reservation.noOfGuests.children
-    } guest(s). Head to your dashboard for more details`,
-  };
-  const newNotification = await createNotification(notification);
-  const socketId = onlineUsers.get(reservation.partner.toString());
-  if (socketId) res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
-  res.locals.io?.emit('update_reservation', { reservationId, status: Status.CANCELLED });
-  return res.sendStatus(204);
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const reservation = await updateReservation(reservationId, false, id, { status: Status.CANCELLED });
+    if (!reservation) throw new NotFoundError('Reservation not found');
+    const populatedProperty: any = await reservation.populate({
+      path: 'property',
+      select: 'type name -_id',
+    });
+
+    const notification = {
+      recipient: reservation.partner,
+      recipientType: EntityType.ESTABLISHMENT,
+      type: NotificationType.RESERVATION,
+      title: 'Reservation Cancelled',
+      message: `A reservation (Ref: ${reservation.reservationRef}) at your ${
+        populatedProperty.property.type ?? 'Restaurant'
+      } — ${populatedProperty.property.name} has been cancelled. The reservation was for ${dayjs(
+        reservation.checkInDay
+      ).format('dddd, MMMM D, YYYY [at] h:mm A')}, with ${
+        reservation.noOfGuests.adults + reservation.noOfGuests.children
+      } guest(s). Head to your dashboard for more details`,
+    };
+    const newNotification = await createNotification(notification);
+    await session.commitTransaction();
+    session.endSession();
+
+    const socketId = onlineUsers.get(reservation.partner.toString());
+    if (socketId) res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
+    res.locals.io?.emit('update_reservation', { reservationId, status: Status.CANCELLED });
+    return res.sendStatus(204);
+  } catch (err: any) {
+    await session.abortTransaction();
+    session.endSession();
+    throw err;
+  }
 });
 
 export const updateReservationHandler = asyncWrapper(
   async (req: Request<{}, {}, updateReservationInput>, res: Response) => {
     const { id } = res.locals.user;
     const { id: reservationId, status } = req.body;
-    const reservation = await updateReservation(reservationId, true, id, { status });
-    if (!reservation) throw new NotFoundError('Reservation not found');
-    const populatedProperty: any = await reservation.populate({
-      path: 'property',
-      select: 'type name -_id',
-    });
-    const notification = {
-      recipient: reservation.user,
-      recipientType: EntityType.USER,
-      type: NotificationType.RESERVATION,
-      title: `Reservation ${reservation.status}`,
-      message: `Your reservation (Ref: ${reservation.reservationRef}) at ${
-        populatedProperty.property.name
-      } is ${reservation.status.toLowerCase()}. The reservation was for ${dayjs(reservation.checkInDay).format(
-        'dddd, MMMM D, YYYY [at] h:mm A'
-      )}, with ${
-        reservation.noOfGuests.adults + reservation.noOfGuests.children
-      } guest(s). Head to your dashboard for more details`,
-    };
-    const newNotification = await createNotification(notification);
-    const socketId = onlineUsers.get(reservation.user.toString());
-    if (socketId) res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
-    res.locals.io?.emit('update_reservation', { reservationId, status });
-    return res.sendStatus(204);
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const reservation = await updateReservation(reservationId, true, id, { status });
+      if (!reservation) throw new NotFoundError('Reservation not found');
+      const populatedProperty: any = await reservation.populate({
+        path: 'property',
+        select: 'type name -_id',
+      });
+
+      const notification = {
+        recipient: reservation.user,
+        recipientType: EntityType.USER,
+        type: NotificationType.RESERVATION,
+        title: `Reservation ${reservation.status}`,
+        message: `Your reservation (Ref: ${reservation.reservationRef}) at ${
+          populatedProperty.property.name
+        } is ${reservation.status.toLowerCase()}. The reservation was for ${dayjs(reservation.checkInDay).format(
+          'dddd, MMMM D, YYYY [at] h:mm A'
+        )}, with ${
+          reservation.noOfGuests.adults + reservation.noOfGuests.children
+        } guest(s). Head to your dashboard for more details`,
+      };
+      const newNotification = await createNotification(notification);
+      await session.commitTransaction();
+      session.endSession();
+
+      const socketId = onlineUsers.get(reservation.user.toString());
+      if (socketId) res.locals.io?.to(socketId).emit('new_notification', omit(newNotification.toJSON(), privateFields));
+      res.locals.io?.emit('update_reservation', { reservationId, status });
+      return res.sendStatus(204);
+    } catch (err: any) {
+      await session.abortTransaction();
+      session.endSession();
+      throw err;
+    }
   }
 );
 
--- src/controllers/review.controllers.ts
+++ src/controllers/review.controllers.ts
@@ -1,35 +1,57 @@
 import { BadRequestError, NotFoundError } from '@errors';
 import { privateFields } from '@models';
 import { canReviewInput, createReviewInput, deleteReviewInput, getPropertyReviewInput } from '@schemas';
-import { canReview, createNotification, createReview, deleteReview, getReviews } from '@services';
+import {
+  canReview,
+  createNotification,
+  createReview,
+  deleteReview,
+  getReviews,
+  updatePropertyReviewDetail,
+} from '@services';
 import { EntityType, NotificationType } from '@types';
 import { asyncWrapper } from '@utils';
 import { Request, Response } from 'express';
 import { omit } from 'lodash';
+import mongoose from 'mongoose';
 
 export const createReviewHandler = asyncWrapper(async (req: Request<{}, {}, createReviewInput>, res: Response) => {
   const { id } = res.locals.user;
-  const { property, ...body } = req.body;
-  const data = { ...body, property, user: id };
-  const isValid = await canReview(property, data.propertyType, id);
-  if (!isValid) throw new BadRequestError('You cannot review this property');
-  const review = await createReview(data);
-  const reviewResponse = omit(review.toJSON(), privateFields);
-  res.status(201).json({ review: reviewResponse });
-  const populatedReview: any = await review.populate({ path: 'property', select: 'name type -_id' });
-  const notification = {
-    recipient: populatedReview.property.partner.id,
-    recipientType: EntityType.ESTABLISHMENT,
-    type: NotificationType.REVIEW,
-    title: 'New Review',
-    message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
-      populatedReview.property.name
-    } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
-  };
-  const newNotification = await createNotification(notification);
-  const socketId = onlineUsers.get(populatedReview.property.partner.id);
-  if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
-  res.locals.io?.emit('new_review', { property, type: body.propertyType, review: reviewResponse });
+  const data = { ...req.body, user: id };
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const isValid = await canReview(data.property, data.propertyType, id);
+    if (!isValid) throw new BadRequestError('You cannot review this property');
+    const review = await createReview(data);
+    const reviewResponse = omit(review.toJSON(), privateFields);
+    const populatedReview: any = await review.populate({ path: 'property', select: 'name type partner -_id' });
+
+    const notification = {
+      recipient: populatedReview.property.partner,
+      recipientType: EntityType.ESTABLISHMENT,
+      type: NotificationType.REVIEW,
+      title: 'New Review',
+      message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
+        populatedReview.property.name
+      } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
+    };
+    const [newNotification] = await Promise.all([
+      createNotification(notification),
+      updatePropertyReviewDetail(data.property, data.propertyType),
+    ]);
+    await session.commitTransaction();
+    session.endSession();
+
+    const socketId = onlineUsers.get(populatedReview.property.partner);
+    if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
+    res.locals.io?.emit('new_review', { property: data.property, type: data.propertyType, review: reviewResponse });
+    return res.status(201).json({ review: reviewResponse });
+  } catch (err: any) {
+    await session.abortTransaction();
+    session.endSession();
+    throw err;
+  }
 });
 
 export const getPropertyReviewsHandler = asyncWrapper(async (req: Request<getPropertyReviewInput>, res: Response) => {
@@ -48,21 +70,36 @@ export const getCanReviewHandler = asyncWrapper(async (req: Request<{}, {}, {}, 
 export const deleteReviewHandler = asyncWrapper(async (req: Request<deleteReviewInput>, res: Response) => {
   const { id } = res.locals.user;
   const { reviewId } = req.params;
-  const review = await deleteReview(reviewId, id);
-  if (!review) throw new NotFoundError('Review not found');
-  res.sendStatus(204);
-  const populatedReview: any = await review.populate({ path: 'property', select: 'name type -_id' });
-  const notification = {
-    recipient: populatedReview.property.partner.id,
-    recipientType: EntityType.ESTABLISHMENT,
-    type: NotificationType.REVIEW,
-    title: 'New Review',
-    message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
-      populatedReview.property.name
-    } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
-  };
-  const newNotification = await createNotification(notification);
-  const socketId = onlineUsers.get(populatedReview.property.partner.id);
-  if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
-  res.locals.io?.emit('delete_review', { property: review.property.id, type: review.propertyType, reviewId });
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const review = await deleteReview(reviewId, id);
+    if (!review) throw new NotFoundError('Review not found');
+    const populatedReview: any = await review.populate({ path: 'property', select: 'name type partner -_id' });
+
+    const notification = {
+      recipient: populatedReview.property.partner,
+      recipientType: EntityType.ESTABLISHMENT,
+      type: NotificationType.REVIEW,
+      title: 'New Review',
+      message: `Your ${populatedReview.property.type ?? 'Restaurant'} — ${
+        populatedReview.property.name
+      } has recieved a new review! Check out what your guest has to say. Visit your dashboard for more details`,
+    };
+    const [newNotification] = await Promise.all([
+      createNotification(notification),
+      updatePropertyReviewDetail(review.property.id, review.propertyType),
+    ]);
+    await session.commitTransaction();
+    session.endSession();
+
+    const socketId = onlineUsers.get(populatedReview.property.partner);
+    if (socketId) res.locals.io?.to(socketId).emit('new_notification', newNotification);
+    res.locals.io?.emit('delete_review', { property: review.property.id, type: review.propertyType, reviewId });
+    return res.sendStatus(204);
+  } catch (err: any) {
+    await session.abortTransaction();
+    session.endSession();
+    throw err;
+  }
 });
