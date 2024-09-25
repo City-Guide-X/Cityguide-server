6ac92b6bb941e4eee3d2920dc0bb8cbb0ed80fea
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-25 20:09:15 +0100

    Update the review routes to allow only users to access them

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -1,5 +1,5 @@
 import { createReviewHandler, deleteReviewHandler, getCanReviewHandler, getPropertyReviewsHandler } from '@controllers';
-import { requireAuth, validateSchema } from '@middlewares';
+import { requireAuth, userOnly, validateSchema } from '@middlewares';
 import { canReviewSchema, createReviewSchema, deleteReviewSchema, getPropertyReviewSchema } from '@schemas';
 import { Router } from 'express';
 
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/can-review', requireAuth, validateSchema(canReviewSchema), getCanReviewHandler);
 router.get('/:propertyId', validateSchema(getPropertyReviewSchema), getPropertyReviewsHandler);
 router.use(requireAuth);
-router.post('/', validateSchema(createReviewSchema), createReviewHandler);
-router.delete('/:reviewId', validateSchema(deleteReviewSchema), deleteReviewHandler);
+router.post('/', userOnly, validateSchema(createReviewSchema), createReviewHandler);
+router.delete('/:reviewId', userOnly, validateSchema(deleteReviewSchema), deleteReviewHandler);
 
 export default router;
