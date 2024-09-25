819044f447bd171c808c927d00d89cf5ef64c27f
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-25 05:28:58 +0100

    Add the can review schema

--- src/schemas/review.schemas.ts
+++ src/schemas/review.schemas.ts
@@ -13,12 +13,16 @@ export const createReviewSchema = object({
     message: string({ required_error: 'Message is required' }).min(10, 'Message requires atleast 10 characters'),
   }).superRefine((data, ctx) => {
     const requiredCategories = categoryMap[data.propertyType];
-    const providedCategories = new Set(Object.keys(data.categoryRatings));
-    const invalidCategories = requiredCategories.filter((category) => !providedCategories.has(category));
-    if (invalidCategories.length > 0 || providedCategories.size !== requiredCategories.length) {
+    const providedCategories = Object.keys(data.categoryRatings);
+    const [provided, required] = [new Set(providedCategories), new Set(requiredCategories)];
+    const invalidCategories = providedCategories.filter((category) => !required.has(category));
+    const missingCategories = requiredCategories.filter((category) => !provided.has(category));
+    if (invalidCategories.length > 0 || providedCategories.length !== requiredCategories.length) {
       ctx.addIssue({
         code: ZodIssueCode.custom,
-        message: `Invalid categories provided. Missing categories are ${invalidCategories.join(', ')}`,
+        message: `Invalid categories provided ${invalidCategories.join(
+          ', '
+        )}. Missing categories are ${missingCategories.join(', ')}`,
         path: ['categoryRatings'],
       });
     }
@@ -37,6 +41,17 @@ export const deleteReviewSchema = object({
   }),
 });
 
+export const canReviewSchema = object({
+  query: object({
+    propertyId: string({ required_error: 'Property id is required' }),
+    propertyType: nativeEnum(PropertyType, {
+      required_error: 'Property type is required',
+      invalid_type_error: 'Property type should be a Stay | Restaurant | NightLife',
+    }),
+  }),
+});
+
 export type createReviewInput = TypeOf<typeof createReviewSchema>['body'];
 export type getPropertyReviewInput = TypeOf<typeof getPropertyReviewSchema>['params'];
 export type deleteReviewInput = TypeOf<typeof deleteReviewSchema>['params'];
+export type canReviewInput = TypeOf<typeof canReviewSchema>['query'];
