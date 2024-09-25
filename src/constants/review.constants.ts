bca5344bf3b7c9130b332a7dfd91794038e6a789
Toby2507 <oluwatobisalau2000@gmail.com>
2024-09-25 20:11:12 +0100

    Add the default categoryReviews for each property

--- src/constants/review.constants.ts
+++ src/constants/review.constants.ts
@@ -29,3 +29,31 @@ export const categoryMap = {
     'Safety',
   ],
 };
+
+export const defaultStayCatReviews = {
+  Staff: 0.0,
+  Facilities: 0.0,
+  Cleanliness: 0.0,
+  Comfort: 0.0,
+  'Value for Money': 0.0,
+  Location: 0.0,
+  'Check-in/Check-out Process': 0.0,
+};
+export const defaultRestaurantCatReviews = {
+  'Food Quality': 0.0,
+  'Menu Variety': 0.0,
+  Service: 0.0,
+  Ambiance: 0.0,
+  'Value for Money': 0.0,
+  Cleanliness: 0.0,
+  Location: 0.0,
+};
+export const defaultNightLifeCatReviews = {
+  Atmosphere: 0.0,
+  'Drink Quality/Selection': 0.0,
+  'Music/Entertainment': 0.0,
+  'Staff/Service': 0.0,
+  Crowd: 0.0,
+  'Value for Money': 0.0,
+  Safety: 0.0,
+};
