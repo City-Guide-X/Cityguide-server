d64e0c854a3f42ed1f93c042dae3425e12cbc44c
Toby2507 <oluwatobisalau2000@gmail.com>
2024-10-02 15:29:43 +0100

    Add the get admin analytics service

--- src/services/misc.service.ts
+++ src/services/misc.service.ts
@@ -1,6 +1,7 @@
-import { NightLifeModel, RestaurantModel, StayModel } from '@models';
-import { IPayload, PropertyType } from '@types';
+import { EstablishmentModel, UserModel } from '@models';
+import { EntityType, IPayload } from '@types';
 import { signJWT } from '@utils';
+import { Types } from 'mongoose';
 
 interface IToken extends IPayload {
   token?: 'access' | 'refresh' | 'both';
@@ -21,8 +22,89 @@ export const signTokens = ({ id, type, isPartner, token = 'both' }: IToken) => {
   return { accessToken, refreshToken };
 };
 
-export const isPropertyType = async (_id: string, type: PropertyType) => {
-  if (type === PropertyType.STAY) return await StayModel.exists({ _id });
-  if (type === PropertyType.RESTAURANT) return await RestaurantModel.exists({ _id });
-  if (type === PropertyType.NIGHTLIFE) return await NightLifeModel.exists({ _id });
+export const getAdminAnalytics = async (id: string, type: EntityType) => {
+  const Model = type === EntityType.USER ? UserModel : EstablishmentModel;
+  const analytics = await Model.aggregate([
+    { $match: { _id: new Types.ObjectId(id) } },
+    {
+      $facet: {
+        stays: [
+          { $lookup: { from: 'stays', localField: '_id', foreignField: 'partner', as: 'stays' } },
+          { $project: { stays: { $size: '$stays' } } },
+        ],
+        restaurants: [
+          { $lookup: { from: 'restaurants', localField: '_id', foreignField: 'partner', as: 'restaurants' } },
+          { $project: { restaurants: { $size: '$restaurants' } } },
+        ],
+        nightlifes: [
+          { $lookup: { from: 'nightlifes', localField: '_id', foreignField: 'partner', as: 'nightlifes' } },
+          { $project: { nightlifes: { $size: '$nightlifes' } } },
+        ],
+        reservation: [
+          {
+            $lookup: {
+              from: 'reservations',
+              localField: '_id',
+              foreignField: 'partner',
+              as: 'reservations',
+              pipeline: [
+                { $group: { _id: '$status', count: { $sum: 1 } } },
+                { $project: { _id: 0, k: '$_id', v: '$count' } },
+              ],
+            },
+          },
+          { $project: { reservation: { $arrayToObject: '$reservations' } } },
+        ],
+        engagements: [
+          { $lookup: { from: 'stays', localField: '_id', foreignField: 'partner', as: 'stays' } },
+          { $lookup: { from: 'restaurants', localField: '_id', foreignField: 'partner', as: 'restaurants' } },
+          { $lookup: { from: 'nightlifes', localField: '_id', foreignField: 'partner', as: 'nightlifes' } },
+          {
+            $lookup: {
+              from: 'reservations',
+              localField: '_id',
+              foreignField: 'partner',
+              as: 'reservations',
+              pipeline: [{ $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'user' } }],
+            },
+          },
+          {
+            $lookup: {
+              from: 'reviews',
+              let: { properties: { $concatArrays: ['$stays', '$restaurants', '$nightlifes'] } },
+              pipeline: [
+                { $match: { $expr: { $in: ['$property', '$$properties._id'] } } },
+                { $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'user' } },
+              ],
+              as: 'reviews',
+            },
+          },
+          {
+            $project: {
+              engagements: {
+                $slice: [
+                  {
+                    $sortArray: { input: { $concatArrays: ['$reservations', '$reviews'] }, sortBy: { updatedAt: -1 } },
+                  },
+                  0,
+                  6,
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        stays: { $arrayElemAt: ['$stays.stays', 0] },
+        restaurants: { $arrayElemAt: ['$restaurants.restaurants', 0] },
+        nightlifes: { $arrayElemAt: ['$nightlifes.nightlifes', 0] },
+        reservation: { $arrayElemAt: ['$reservation.reservation', 0] },
+        engagements: { $arrayElemAt: ['$engagements.engagements', 0] },
+      },
+    },
+  ]);
+  return analytics[0];
 };
