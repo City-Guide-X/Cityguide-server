5a37677fde73267b20ad93911cfd553f13eb71f1
Toby2507 <oluwatobisalau2000@gmail.com>
2024-10-21 10:06:44 +0100

    Add the exchange rate schema

--- src/schemas/payment.schemas.ts
+++ src/schemas/payment.schemas.ts
@@ -2,12 +2,20 @@ import { number, object, string, TypeOf } from 'zod';
 
 export const initiatePaymentSchema = object({
   body: object({
-    amount: number({ required_error: 'Amount is required', invalid_type_error: 'Amount should be a number' }),
-    currency: string({ required_error: 'Currency is required' }).regex(
-      /^NGN|USD|GHS|ZAR|KES$/,
-      'Currency should be a valid currency code'
-    ),
+    amount: number({ invalid_type_error: 'Amount should be a number' }).optional(),
+  }),
+});
+
+export const exchangeRateSchema = object({
+  query: object({
+    base: string({ required_error: 'Base currency is required' }).regex(/^[A-Z]{3}$/, 'Invalid base currency'),
+    currency: string({ required_error: 'Currency is required' }).regex(/^[A-Z]{3}$/, 'Invalid currency'),
+    amount: string()
+      .regex(/^\d+(\.\d{2})?$/, 'Invalid amount')
+      .optional()
+      .transform((val) => val && parseFloat(val)),
   }),
 });
 
 export type initiatePaymentInput = TypeOf<typeof initiatePaymentSchema>['body'];
+export type exchangeRateInput = TypeOf<typeof exchangeRateSchema>['query'];
