de79e8b834238fcce0cf8edacb3b721f39a34e7f
Toby2507 <oluwatobisalau2000@gmail.com>
2024-11-09 22:23:12 +0100

    Add the vtu transaction creation schema

--- src/schemas/vtu.schemas.ts
+++ src/schemas/vtu.schemas.ts
@@ -1,5 +1,5 @@
-import { ISPs } from '@types';
-import { nativeEnum, object, string, TypeOf } from 'zod';
+import { ISPs, VTUType } from '@types';
+import { boolean, nativeEnum, number, object, string, TypeOf } from 'zod';
 
 export const createReceiverSchema = object({
   body: object({
@@ -34,6 +34,30 @@ export const deleteReceiverSchema = object({
   }),
 });
 
+export const createTransactionSchema = object({
+  body: object({
+    firstName: string({ required_error: 'First name is required' }).min(1, 'First name should be atleast 1 character'),
+    lastName: string({ required_error: 'Last name is required' }).min(1, 'Last name should be atleast 1 character'),
+    phoneNumber: string({ required_error: 'Phone number is required' }).min(
+      11,
+      'Phone number should be atleast 11 characters'
+    ),
+    network: nativeEnum(ISPs, {
+      required_error: 'Network is required',
+      invalid_type_error: 'Invalid network',
+    }),
+    amount: number({ required_error: 'Amount is required', invalid_type_error: 'Amount should be a number' }).min(1),
+    type: nativeEnum(VTUType, {
+      required_error: 'Transaction type is required',
+      invalid_type_error: 'Invalid transaction type',
+    }),
+    payReference: string().optional(),
+    useSavedCard: boolean({ invalid_type_error: 'Save card should be true or false' }).optional().default(false),
+    saveCard: boolean({ invalid_type_error: 'Save card should be true or false' }).optional().default(true),
+  }),
+});
+
 export type createReceiverInput = TypeOf<typeof createReceiverSchema>['body'];
 export type updateReceiverInput = TypeOf<typeof updateReceiverSchema>;
 export type deleteReceiverInput = TypeOf<typeof deleteReceiverSchema>['params'];
+export type createTransactionInput = TypeOf<typeof createTransactionSchema>['body'];
