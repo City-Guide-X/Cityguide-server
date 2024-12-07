4efcd8b3113833c0f54cdbc82a80bce7ee9320cc
Toby2507 <oluwatobisalau2000@gmail.com>
2024-12-07 11:26:46 +0100

    Update the create review schema to accept the reviewer field and an optional reservationId field which is used to populate the accommodation and stayDuration field in the review model

--- src/schemas/review.schemas.ts
+++ src/schemas/review.schemas.ts
@@ -1,5 +1,5 @@
 import { categoryMap } from '@constants';
-import { PropertyType, Rating } from '@types';
+import { PropertyType, Rating, Reviewer } from '@types';
 import { nativeEnum, object, string, TypeOf, ZodIssueCode } from 'zod';
 
 export const createReviewSchema = object({
@@ -10,7 +10,12 @@ export const createReviewSchema = object({
       invalid_type_error: 'Property type should be a Stay | Restaurant | NightLife',
     }),
     categoryRatings: object({}, { required_error: 'Category rating is required' }).catchall(nativeEnum(Rating)),
-    message: string({ required_error: 'Message is required' }),
+    message: string({ required_error: 'Message is required' }).min(3, 'Message should be at least 3 characters'),
+    reservationId: string().optional(),
+    reviewer: nativeEnum(Reviewer, {
+      required_error: 'Reviewer is required',
+      invalid_type_error: 'Reviewer should be a Family | Couple | Solo traveler | Business traveler | Group of friends',
+    }),
   }).superRefine((data, ctx) => {
     const requiredCategories = categoryMap[data.propertyType];
     const providedCategories = Object.keys(data.categoryRatings);
@@ -24,6 +29,13 @@ export const createReviewSchema = object({
           ', '
         )}. Missing categories are ${missingCategories.join(', ')}`,
         path: ['categoryRatings'],
+      });
+    }
+    if (data.propertyType === PropertyType.STAY && !data.reservationId) {
+      ctx.addIssue({
+        code: ZodIssueCode.custom,
+        message: 'Reservation id is required for Stay property',
+        path: ['reservationId', 'propertyType'],
       });
     }
   }),
